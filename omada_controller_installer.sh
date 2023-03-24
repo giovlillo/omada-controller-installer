0a4358b30eddd811ef50bd87bb233648942711a3
giovlillo <47029332+giovlillo@users.noreply.github.com>
2023-03-25 00:36:48 +0100

    Update omada_controller_installer.sh

--- omada_controller_installer.sh
+++ omada_controller_installer.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
-#title           :omada-controller-installer.sh
-#description     :Automated installation of the latest version of Omada Controller
-#supported       :Ubuntu 16.04, Ubuntu 18.04, Ubuntu 20.04
-#updated         :24/03/2023
 
-# Check if the script is running on a supported version of Ubuntu
-if ! [[ $(lsb_release -rs) =~ ^(16.04|18.04|20.04)$ ]]; then
-  echo "This script only supports Ubuntu 16.04, 18.04, and 20.04."
-  exit 1
+# Check if user is root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
 fi
 
-# Install MongoDB 4.4 and OpenJDK-8
-echo "Installing MongoDB 4.4 and OpenJDK-8..."
-wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
-sudo apt-get update -qq
-sudo apt-get install -y openjdk-8-jre-headless mongodb-org jsvc curl
+# Check if running on supported Ubuntu versions
+if [[ $(lsb_release -rs) != "16.04" && $(lsb_release -rs) != "18.04" && $(lsb_release -rs) != "20.04" ]]; then
+    echo "This script can only be run on Ubuntu 16.04, 18.04, or 20.04"
+    exit 1
+fi
 
-# Install the latest version of Omada Controller
-echo "Installing the latest version of Omada Controller..."
-OmadaPackageUrl=$(curl -s https://www.tp-link.com/support/download/omada-software-controller/ | grep -oP '(?<=href=")[^"]*Omada_SDN_Controller_v\d+\.\d+\.\d+_Linux_x64\.deb[^"]*')
-wget $OmadaPackageUrl -P /tmp/
-sudo dpkg -i /tmp/$(basename $OmadaPackageUrl)
+# Install required dependencies
+apt-get update && apt-get install -y curl openjdk-8-jdk jsvc
 
-# Show IP address of the machine
-ip_address=$(hostname -I | awk '{print $1}')
-echo "Installation complete! You can access the Omada Controller at https://${ip_address}:8043"
+# Add MongoDB repository and install MongoDB 4.4
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/4.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+apt-get update && apt-get install -y mongodb-org
+
+# Download Omada SDN Controller package
+curl -s https://www.tp-link.com/en/support/download/omada-software-controller/ | grep -Eo 'https://static.tp-link.com/'$(curl -s https://www.tp-link.com/en/support/download/omada-software-controller/ | grep -Eo 'Omada_SDN_Controller_v[0-9]+\.[0-9]+\.[0-9]+_Linux_x64\.deb')'[a-zA-Z0-9/\._-]+' | xargs curl -O
+
+# Install Omada SDN Controller package
+dpkg -i Omada_SDN_Controller_*.deb
+
+# Start Omada SDN Controller service
+systemctl start omada.service
+
+# Show IP address and port to access Omada SDN Controller web interface
+IP=$(ip -4 route get 8.8.8.8 | awk '{print $7}')
+echo "Omada SDN Controller is running at http://$IP:8088"
